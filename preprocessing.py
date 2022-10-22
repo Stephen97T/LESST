2480dcd83b9cd6379e06a377f983c566556555f8
Stephen Tseng <s.tseng@watsonholmes.nl>
2022-10-22 15:43:59 +0200

    added functions in preprocessing for preparing all data files and extracting tsfeatures, still need to incorporate train/validation split

--- preprocessing.py
+++ preprocessing.py
@@ -1,85 +1,49 @@
+import os
+from time import time
+
+os.environ["R_HOME"] = "/Users/steph/miniconda3/envs/bay/lib/R"
+from typing import List
+from tsfeatures.tsfeatures_r import tsfeatures_r
+import pandas as pd
+from rpy2.robjects.packages import importr, data
+
+# utils = importr("utils")
+# utils.install_packages("data.table")
+# utils.install_packages("tsfeatures")
+from tsfeatures.tsfeatures_r import tsfeatures_r
+from tsfeatures.tsfeatures import tsfeatures
+from tsfeatures.m4_data import *
+import rpy2.robjects as ro
+from rpy2.robjects import pandas2ri
+from rpy2.robjects.conversion import localconverter
 import pandas as pd
 
+# import os
+# os.environ["R_HOME"] = "/Users/steph/miniconda3/envs/bay/lib/R"
+from tsfeatures.m4_data import *
 
-def prepare_m4data(data, dataset_name, infopath="C:/thesis/data/m4"):
-    seas_dict = {
-        "Hourly": {
-            "seasonality": 24,
-            "input_size": 24,
-            "output_size": 48,
-            "freq": "H",
-        },
-        "Daily": {
-            "seasonality": 7,
-            "input_size": 7,
-            "output_size": 14,
-            "freq": "D",
-        },
-        "Weekly": {
-            "seasonality": 52,
-            "input_size": 52,
-            "output_size": 13,
-            "freq": "W",
-        },
-        "Monthly": {
-            "seasonality": 12,
-            "input_size": 12,
-            "output_size": 18,
-            "freq": "M",
-        },
-        "Quarterly": {
-            "seasonality": 4,
-            "input_size": 4,
-            "output_size": 8,
-            "freq": "Q",
-        },
-        "Yearly": {
-            "seasonality": 1,
-            "input_size": 4,
-            "output_size": 6,
-            "freq": "D",
-        },
-    }
-    freq = seas_dict[dataset_name]["freq"]
-    m4_info = pd.read_csv(
-        infopath + "/M4-info.csv", usecols=["M4id", "category"]
-    )
-    m4_info = m4_info[
-        m4_info["M4id"].str.startswith(dataset_name[0])
-    ].reset_index(drop=True)
-    df = data.copy()
-    df = df.rename(columns={"V1": "unique_id"})
-    df = pd.wide_to_long(
-        df, stubnames=["V"], i="unique_id", j="ds"
-    ).reset_index()
-    df = df.rename(columns={"V": "y"})
-    df = df.dropna()
-    df["split"] = "train"
-    df["ds"] = df["ds"] - 1
-    # Get len of series per unique_id
-    len_series = df.groupby("unique_id").agg({"ds": "max"}).reset_index()
-    len_series.columns = ["unique_id", "len_serie"]
-    len_series = df.groupby("unique_id").agg({"ds": "max"}).reset_index()
-    dates = []
-    for i in range(len(len_series)):
-        len_serie = len_series.iloc[i, 1]
-        ranges = pd.date_range(
-            start="1970/01/01", periods=len_serie, freq=freq
-        )
-        dates += list(ranges)
-    df.loc[:, "ds"] = dates
 
-    df = df.merge(m4_info, left_on=["unique_id"], right_on=["M4id"])
-    df.drop(columns=["M4id"], inplace=True)
-    df = df.rename(columns={"category": "x"})
+def prepare_allm4data(
+    outputfolder="C:/thesis/data/m4/processed", datapath="C:/thesis/data/"
+):
+    datasets = ["Hourly", "Daily", "Weekly", "Monthly", "Quarterly", "Yearly"]
+    for dset in datasets:
+        xtrain, ytrain, xtest, ytest = m4_parser(dset, datapath)
+        xtrain.to_csv(f"{outputfolder}/train/xtrain-{dset.lower()}.csv")
+        ytrain.to_csv(f"{outputfolder}/train/ytrain-{dset.lower()}.csv")
+        xtest.to_csv(f"{outputfolder}/test/xtest-{dset.lower()}.csv")
+        ytest.to_csv(f"{outputfolder}/test/ytest-{dset.lower()}.csv")
 
-    X_train_df = df[df["split"] == "train"].filter(
-        items=["unique_id", "ds", "x"]
-    )
-    y_train_df = df[df["split"] == "train"].filter(
-        items=["unique_id", "ds", "y"]
-    )
 
-    X_train_df = X_train_df.reset_index(drop=True)
-    y_train_df = y_train_df.reset_index(drop=True)
-    return X_train_df, y_train_df
+def prepare_m4tsfeatures(
+    filename="features_train_val",
+    outputfolder="C:/thesis/data/m4/processed/tsfeatures",
+    datapath="C:/thesis/data/m4/processed/train",
+):
+    datasets = ["Hourly", "Daily", "Weekly", "Monthly", "Quarterly", "Yearly"]
+    for dset in datasets:
+        y = pd.read_csv(f"{datapath}/ytrain-{dset.lower()}.csv", index_col=0)
+        features = tsfeatures_r(y, freq=1)
+        with localconverter(ro.default_converter + pandas2ri.converter):
+            features = ro.conversion.rpy2py(features)
+        features.to_csv(f"{outputfolder}/{filename}-{dset.lower()}.csv")
--- timeseries.py
+++ timeseries.py
@@ -1,171 +1,33 @@
 #!/usr/bin/env python
 # coding: utf-8
 import os
+from time import time
 
 os.environ["R_HOME"] = "/Users/steph/miniconda3/envs/bay/lib/R"
 from typing import List
-
-
+from tsfeatures.tsfeatures_r import tsfeatures_r
 import pandas as pd
-import rpy2.robjects as robjects
+from rpy2.robjects.packages import importr, data
+
+# utils = importr("utils")
+# utils.install_packages("data.table")
+# utils.install_packages("tsfeatures")
+from tsfeatures.tsfeatures_r import tsfeatures_r
+from tsfeatures.tsfeatures import tsfeatures
+from tsfeatures.m4_data import *
+import rpy2.robjects as ro
 from rpy2.robjects import pandas2ri
+from rpy2.robjects.conversion import localconverter
 
-
-def tsfeatures_r(
-    ts: pd.DataFrame,
-    freq: int,
-    features: List[str] = [
-        "length",
-        "acf_features",
-        "arch_stat",
-        "crossing_points",
-        "entropy",
-        "flat_spots",
-        "heterogeneity",
-        "holt_parameters",
-        "hurst",
-        "hw_parameters",
-        "lumpiness",
-        "nonlinearity",
-        "pacf_features",
-        "stability",
-        "stl_features",
-        "unitroot_kpss",
-        "unitroot_pp",
-    ],
-    **kwargs
-) -> pd.DataFrame:
-    """tsfeatures wrapper using r.
-    Parameters
-    ----------
-    ts: pandas df
-        Pandas DataFrame with columns ['unique_id', 'ds', 'y'].
-        Long panel of time series.
-    freq: int
-        Frequency of the time series.
-    features: List[str]
-        String list of features to calculate.
-    **kwargs:
-        Arguments used by the original tsfeatures function.
-    References
-    ----------
-    https://pkg.robjhyndman.com/tsfeatures/reference/tsfeatures.html
-    """
-    rstring = """
-        function(df, freq, features, ...){
-          suppressMessages(library(data.table))
-          suppressMessages(library(tsfeatures))
-          dt <- as.data.table(df)
-          setkey(dt, unique_id)
-          series_list <- split(dt, by = "unique_id", keep.by = FALSE)
-          series_list <- lapply(series_list,
-                                function(serie) serie[, ts(y, frequency = freq)])
-          if("hw_parameters" %in% features){
-            features <- setdiff(features, "hw_parameters")
-            if(length(features)>0){
-                hw_series_features <- suppressMessages(tsfeatures(series_list, "hw_parameters", ...))
-                names(hw_series_features) <- paste0("hw_", names(hw_series_features))
-                series_features <- suppressMessages(tsfeatures(series_list, features, ...))
-                series_features <- cbind(series_features, hw_series_features)
-            } else {
-                series_features <- suppressMessages(tsfeatures(series_list, "hw_parameters", ...))
-                names(series_features) <- paste0("hw_", names(series_features))
-            }
-          } else {
-            series_features <- suppressMessages(tsfeatures(series_list, features, ...))
-          }
-          setDT(series_features)
-          series_features[, unique_id := names(series_list)]
-        }
-    """
-    pandas2ri.activate()
-    rfunc = robjects.r(rstring)
-
-    feats = rfunc(ts, freq, features, **kwargs)
-    pandas2ri.deactivate()
-
-    renamer = {"ARCH.LM": "arch_lm", "length": "series_length"}
-    feats = feats.rename(columns=renamer)
-
-    return feats
-
-
-def tsfeatures_r_wide(
-    ts: pd.DataFrame,
-    features: List[str] = [
-        "length",
-        "acf_features",
-        "arch_stat",
-        "crossing_points",
-        "entropy",
-        "flat_spots",
-        "heterogeneity",
-        "holt_parameters",
-        "hurst",
-        "hw_parameters",
-        "lumpiness",
-        "nonlinearity",
-        "pacf_features",
-        "stability",
-        "stl_features",
-        "unitroot_kpss",
-        "unitroot_pp",
-    ],
-    **kwargs
-) -> pd.DataFrame:
-    """tsfeatures wrapper using r.
-    Parameters
-    ----------
-    ts: pandas df
-        Pandas DataFrame with columns ['unique_id', 'seasonality', 'y'].
-        Wide panel of time series.
-    features: List[str]
-        String list of features to calculate.
-    **kwargs:
-        Arguments used by the original tsfeatures function.
-    References
-    ----------
-    https://pkg.robjhyndman.com/tsfeatures/reference/tsfeatures.html
-    """
-    rstring = """
-        function(uids, seasonalities, ys, features, ...){
-            suppressMessages(library(data.table))
-            suppressMessages(library(tsfeatures))
-            suppressMessages(library(purrr))
-            series_list <- pmap(
-                                list(uids, seasonalities, ys),
-                                function(uid, seasonality, y) ts(y, frequency=seasonality)
-                            )
-            names(series_list) <- uids
-            if("hw_parameters" %in% features){
-                features <- setdiff(features, "hw_parameters")
-                if(length(features)>0){
-                    hw_series_features <- suppressMessages(tsfeatures(series_list, "hw_parameters", ...))
-                    names(hw_series_features) <- paste0("hw_", names(hw_series_features))
-                    series_features <- suppressMessages(tsfeatures(series_list, features, ...))
-                    series_features <- cbind(series_features, hw_series_features)
-                } else {
-                    series_features <- suppressMessages(tsfeatures(series_list, "hw_parameters", ...))
-                    names(series_features) <- paste0("hw_", names(series_features))
-                }
-            } else {
-                series_features <- suppressMessages(tsfeatures(series_list, features, ...))
-            }
-            setDT(series_features)
-            series_features[, unique_id := names(series_list)]
-        }
-    """
-    pandas2ri.activate()
-    rfunc = robjects.r(rstring)
-
-    uids = ts["unique_id"].to_list()
-    seasonalities = ts["seasonality"].to_list()
-    ys = ts["y"].to_list()
-
-    feats = rfunc(uids, seasonalities, ys, features, **kwargs)
-    pandas2ri.deactivate()
-
-    renamer = {"ARCH.LM": "arch_lm", "length": "series_length"}
-    feats = feats.rename(columns=renamer)
-
-    return feats
+# xtrain, ytrain, xtest, ytest = m4_parser("Monthly", "C:/thesis/data/")
+# panel = ytrain[ytrain.unique_id == "M1"]
+"""
+t = time()
+b = tsfeatures(panel, freq=1)
+print(f"python finished in {time()-t} sec")
+t = time()
+a = tsfeatures_r(panel, freq=1)
+with localconverter(ro.default_converter + pandas2ri.converter):
+    c = ro.conversion.rpy2py(a)
+print(f"R finished in {time()-t} sec")
+"""
